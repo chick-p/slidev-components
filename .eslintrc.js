31e968c98924e260b729d36880e65af23f3fbea8
piyo <chick.p.1120@gmail.com>
2022-06-21 21:44:21 +0900

    chore: setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
+};

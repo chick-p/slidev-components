d79a8a16ec253e91698edf80feeef5aedc5b232f
piyo <chick.p.1120@gmail.com>
2025-06-10 19:05:39 +0900

    style: apply pretteir

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import pluginVue from 'eslint-plugin-vue'
+import pluginVue from "eslint-plugin-vue";
 import {
   defineConfigWithVueTs,
   vueTsConfigs,
-} from '@vue/eslint-config-typescript'
+} from "@vue/eslint-config-typescript";
 
 export default defineConfigWithVueTs(
   js.configs.recommended,
   eslintConfigPrettier,
-  pluginVue.configs['flat/essential'],
+  pluginVue.configs["flat/essential"],
   vueTsConfigs.recommended,
-)
+);
